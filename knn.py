b3e17ce8c631ef323c4a2a143a84e8c943213e4e
joerenner <rennerj2@students.wwu.edu>
2017-10-24 19:39:27 +0200

    implemented directional distance cost to edit distance

--- knn.py
+++ knn.py
@@ -2,13 +2,12 @@ from collections import Counter
 
 # input: two lists of integers representing the freeman codes of two images
 # output: the edit distance between the two lists
-# uses dynamic programming and full m-by-n space table
-def edit_distance(im1, im2):
+# uses dynamic programming and full m-by-n table
+def edit_distance(im1, im2, directions=False):
     m = len(im1)
     n = len(im2)
     # create table for storing results
     distance = [[0 for i in xrange(n+1)] for j in xrange(m+1)]
-
     for i in xrange(m+1):
         for j in xrange(n+1):
             if i == 0:
@@ -18,8 +17,22 @@ def edit_distance(im1, im2):
             elif im1[i-1] == im2[j-1]:
                 distance[i][j] = distance[i-1][j-1]
             else:
-                distance[i][j] = (min(distance[i-1][j], distance[i][j-1], distance[i-1][j-1])) + 1
+                if directions:
+                    distance[i][j] = min(distance[i-1][j] + 1, distance[i][j-1] + 1,
+                                         distance[i-1][j-1] + float(direction_cost(im1[i-1],im2[j-1])) / 8.0)
+                else:
+                    distance[i][j] = (min(distance[i-1][j], distance[i][j-1], distance[i-1][j-1])) + 1
     return distance[m][n]
+
+# function that computes the directional distance between two integers in a freeman code
+# computes the distance clockwise and counterclockwise around direction key, returns minimum
+# input: two integers representing directions from freeman codes
+# output: minimum distance required to change from one direction two another
+def direction_cost(x,y):
+    direction1 = abs(x-y)
+    direction2 = (min(x,y)+ 8) - max(x,y)
+    return min(direction1,direction2)
+
 
 # input: image to be classified, training features, training labels (same order), k
 # output: majority class of k-nearest neighbors to input image
@@ -30,12 +43,15 @@ def knn(im1, X, labels, k):
         distances.append(edit_distance(im1, example))
     knn_classes = [y for _,y in sorted(zip(distances, labels))[:k]]
     counter = Counter(knn_classes)
-    return counter.most_common(4)[0][0]
-
+    return counter.most_common(k)[0][0]
 
 
 
 im1 = [1,2,3]
+im2 = [2,3,4,5,6,7]
+print edit_distance(im1,im2)
+print edit_distance(im1,im2,directions=True)
+exit()
 X = [[1,2,1],[4,5,2],[2,1,2],[1,2,5]]
 labels = [1,2,3,1]
 print knn(im1,X,labels,2)
