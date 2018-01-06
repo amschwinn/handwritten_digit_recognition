f9fee520a0d17ee8a7d048c985826a70b51d55ac
joerenner <rennerj2@students.wwu.edu>
2018-01-06 15:38:35 +0100

    implemented clustering of frequent sequences

--- sequence_mining.py
+++ sequence_mining.py
@@ -2,7 +2,23 @@ from pymining import freq_seq_enum
 from knn import edit_distance
 from sklearn.cluster import DBSCAN
 from numpy import zeros
+import pickle
+from numpy import mean
 
+# Returns true if seq1 is a subsequence of seq2
+# m is length of str1, n is length of str2
+def is_sub_sequence(seq1, seq2):
+    m = len(seq1)
+    n = len(seq2)
+    if m == n:
+        return seq1 == seq2
+    j = 0 # seq1 index
+    i = 0 # seq2 index
+    while j < m and i < n:
+        if seq1[j] == seq2[i]:
+            j = j + 1
+        i = i + 1
+    return j == m
 
 # creates distance matrix for edit distance between examples
 def pairwise_dists(seqs):
@@ -45,18 +61,63 @@ def mine_seqs(codes, minsup, max_gap, min_length):
             del supports[i]
     return freq_seqs, supports
 
+# find representative subsequence for each cluster
+# takes subsequence with smallest mean distance to other subseqs in cluster
+# input: list of sequences, list of corresponding supports, list of corresponding cluster labels, square distance matrix
+# number of sequences for the digit (used when calculating support ratio)
+# output: list of subsequences that represent each cluster, corresponding cluster support
+def merge_clusters(seqs, sups, cluster_labels, dists, num_seq_digit):
+    n = len(sups)
+    clusters = set(cluster_labels)
+    cluster_seq = []
+    cluster_sup = []
+    for cluster in clusters:
+        cands = []  # candidates to represent this cluster
+        for i in range(n):
+            # get indices of subsequences in specific cluster
+            if cluster_labels[i] == cluster:
+                cands.append(i)
+        best_avg_dist = 1000
+        best_index = 0
+        sup = 0  # total support count for all subseqs in cluster
+        for cand in cands:
+            dist = 0
+            for cand2 in cands:
+                if cand != cand2:
+                    dist += dists[cand, cand2]
+            dist = float(dist) / float(len(cands))
+            if dist < best_avg_dist:
+                best_index = cand
+        cluster_seq.append(seqs[best_index])
+        cluster_sup.append(round(mean([sups[x] for x in cands]) / float(num_seq_digit), 2))
+    return cluster_seq, cluster_sup
 
-codes = [[1,4,6,3,7,2,1,9,2,3,4,6,4,6,4,3,7,8],[8,6,4,7,4,8,5,8,6,9,7,0,8,7,8,4,3,7,2,5,1,4,3,3,1,5,2,7,8,4],
-         [1,1,1,1,1,8,2,8,2,9,3,9,3,8,4,4,7,4,7,3,8,3]]
+if __name__ == "__main__":
+    freeman_train = pickle.load(open('processed_data/freeman_train3.sav','r'))
+    freeman_labels = pickle.load(open('processed_data/freeman_labels3.sav','r'))
 
-seqs, sup = mine_seqs(codes, minsup=2, max_gap=2, min_length=5)
+    digit_codes = {}
+    for i in range(len(freeman_labels)):
+        if freeman_labels[i] not in digit_codes:
+            digit_codes[freeman_labels[i]] = [freeman_train[i]]
+        else:
+            digit_codes[freeman_labels[i]].append(freeman_train[i])
 
-#codes = ('2123', '12234', '23345', '34456')
-dists = pairwise_dists(seqs)
-clusterer = DBSCAN(eps=2.0,min_samples=2, metric='precomputed')
-clusterer.fit(dists)
-print clusterer.components_
-print clusterer.labels_
-
-    
-
+    digit = 9
+    min_sup_thresh = 0.55    # minimum support threshold
+    max_gap = 2             # max gap between elements of a subsequence
+    min_len_thresh = 0.4    # minimum length threshold (multiplied by mean length of code to find min length for subseqs)
+    cluster_max_dist = 2.0  # max dist between two example to be considered same neighborhood (for clustering)
+    num_total_seq = len(digit_codes[digit])
+    minsup = int(round(len(digit_codes[digit]) * min_sup_thresh))
+    min_length = int(round(mean([len(x) for x in digit_codes[digit]]) * min_len_thresh))
+    seqs, sup = mine_seqs(digit_codes[digit], minsup=minsup, max_gap=max_gap, min_length=min_length)
+    seq_sup = zip(seqs, sup)
+    seq_sup.sort(key=lambda x: x[1])
+    seqs, sup = zip(*seq_sup)
+    dists = pairwise_dists(seqs)
+    clusterer = DBSCAN(eps=cluster_max_dist,min_samples=2, metric='precomputed')
+    clusterer.fit(dists)
+    final_seqs, final_sups = merge_clusters(seqs,sup,clusterer.labels_,dists,num_total_seq)
+    print final_seqs
+    print final_sups
